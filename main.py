64b1b51c73bb173e0a82b5b3b4d103977c3e3158
ilia ozhmegov <elijahozhmegov@gmail.com>
2019-10-13 19:34:31 +0200

    front-end: add background grid

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 ####
 
 import pygame
+import random
 
 
 ####
 
 
-class PGView(object):
+class SnakeWindow(object):
 
-
-    def __init__(self, width = 640, height = 480, fps = 30):
+    def __init__(self, height=600, fps=42):
         """
         Initialize pygame, window, background, font and etc.
 
@@ -20,7 +20,7 @@ class PGView(object):
         pygame.init()
         pygame.display.set_caption("Press ESC to quit")
 
-        self.width      = width
+        self.width      = 3 * height // 2
         self.height     = height
         self.screen     = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
         self.background = pygame.Surface(self.screen.get_size()).convert()
@@ -30,6 +30,9 @@ class PGView(object):
         self.fps      = fps
         self.playtime = 0.0
         self.font     = pygame.font.SysFont('mono', 20, bold=True)
+
+        self.color1 = 0
+        self.color2 = 0
 
     def paint(self):
         pygame.draw.circle(self.background, (255, 128, 55), (200, 200), 100, 10)
@@ -44,13 +47,36 @@ class PGView(object):
         myball = Ball()
         myball.blit(self.background)
 
+    def draw_cute_pattern(self):
+        for t in range(20):
+            pygame.draw.line(self.screen,
+                             (self.color1, 255 - self.color1, self.color2),
+                             (32*t, 0),
+                             (0, self.height - 24*t))
+            pygame.draw.line(self.screen,
+                             (255 - self.color2, self.color2, self.color1),
+                             (32*t, self.height),
+                             (self.width, self.height - 24*t))
+
+            self.color1 = random.randint(0, 255)
+            self.color2 = random.randint(0, 255)
+
+    def draw_background(self):
+        xpos = self.width // 3
+        ypos = 0
+        xsize = self.height
+        ysize = self.height
+        BG = BackGround(xpos, ypos, xsize, ysize)
+        BG.draw_lines(self.background)
 
     def run(self):
         """
         The mainloop
 
         """
-        self.paint()
+        # self.paint()
+        self.draw_background()
+
         running = True
         while running:
             for event in pygame.event.get():
@@ -60,17 +86,19 @@ class PGView(object):
                     if event.key == pygame.K_ESCAPE:
                         running = False
 
-            milliseconds = self.clock.tick(self.fps)
-            self.playtime += milliseconds / 1000.0;
-
-            fw, fh = self.font.size("AAA")
-            self.draw_text("PLAYTIME: {:^10.3f} seconds".format(self.playtime))
-            self.draw_text("FPS: {:6.3}".format(self.clock.get_fps()), (0, fh))
-
+            self.draw_tech_info()
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
 
         pygame.quit()
+
+    def draw_tech_info(self):
+        milliseconds = self.clock.tick(self.fps)
+        self.playtime += milliseconds / 1000.0
+
+        fw, fh = self.font.size("AAA")
+        self.draw_text("PLAYTIME: {:^10.3f} seconds".format(self.playtime))
+        self.draw_text("FPS: {:6.3}".format(self.clock.get_fps()), (0, fh))
 
     def draw_text(self, text, position=(0, 0)):
         """
@@ -83,16 +111,39 @@ class PGView(object):
         self.screen.blit(surface, position)
 
 
+class BackGround(object):
+
+    def __init__(self, x_pos, y_pos, x_size, y_size, x_pace=20, y_pace=20, colour=(0, 0, 255)):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_size = x_size
+        self.y_size = y_size
+        self.x_pace = x_pace
+        self.y_pace = y_pace
+        self.colour = colour
+
+        self.surface = pygame.Surface((self.x_size, self.y_size))
+
+    def draw_lines(self, background):
+
+        for i in range(self.x_pace, self.x_size, self.x_pace):
+            pygame.draw.line(self.surface, self.colour, (i, 0), (i, self.x_size))
+            pygame.draw.line(self.surface, self.colour, (0, i), (self.y_size, i))
+
+        self.surface = self.surface.convert()
+        background.blit(self.surface, (self.x_pos, self.y_pos))
+
+
 class Ball(object):
 
     def __init__(self, radius=50, colour=(0, 0, 255), x=320, y=240):
         self.radius = radius
-        self.color  = colour
+        self.colour = colour
         self.x = x
         self.y = y
 
         self.surface = pygame.Surface((2 * self.radius, 2 * self.radius))
-        self.surface.set_colorkey((0,0,0))         # make black the transparent color (red,green,blue)
+        self.surface.set_colorkey((0, 0, 0))
         pygame.draw.circle(self.surface, colour, (radius, radius), radius)
         self.radius = self.surface.convert_alpha()
 
@@ -101,7 +152,7 @@ class Ball(object):
 
 
 def main():
-    PGView(600, 400).run()
+    SnakeWindow().run()
 
 
 if __name__ == "__main__":
