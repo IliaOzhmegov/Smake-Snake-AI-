3dc7271dc1793bb7e26f22bd1d82aa82de85e92e
ilia ozhmegov <elijahozhmegov@gmail.com>
2019-10-13 13:04:10 +0200

    experiments: add blue ball

--- main.py
+++ main.py
@@ -24,17 +24,33 @@ class PGView(object):
         self.height     = height
         self.screen     = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
         self.background = pygame.Surface(self.screen.get_size()).convert()
+        self.background.fill((128, 128, 128))
 
         self.clock    = pygame.time.Clock()
         self.fps      = fps
         self.playtime = 0.0
         self.font     = pygame.font.SysFont('mono', 20, bold=True)
 
+    def paint(self):
+        pygame.draw.circle(self.background, (255, 128, 55), (200, 200), 100, 10)
+        pygame.draw.polygon(self.background, (0, 200, 200),
+                            ((100, 100), (200, 100),
+                             (120, 200), (150, 50), (180, 200)), 3)
+        pygame.draw.arc(self.background, (0, 50, 200), (0, 0, self.width, self.height), -3.14/4, 3.14/4 * 3)
+
+        for point in range(0, self.width, 64):  # range(start, stop, step)
+            pygame.draw.line(self.background, (255, 0, point/self.width * 255), (0, 0), (480, point), 1)
+
+        myball = Ball()
+        myball.blit(self.background)
+
+
     def run(self):
         """
         The mainloop
 
         """
+        self.paint()
         running = True
         while running:
             for event in pygame.event.get():
@@ -48,7 +64,7 @@ class PGView(object):
             self.playtime += milliseconds / 1000.0;
 
             fw, fh = self.font.size("AAA")
-            self.draw_text("PLAYTIME: {:>6.3} seconds".format(self.playtime))
+            self.draw_text("PLAYTIME: {:^10.3f} seconds".format(self.playtime))
             self.draw_text("FPS: {:6.3}".format(self.clock.get_fps()), (0, fh))
 
             pygame.display.flip()
@@ -65,6 +81,23 @@ class PGView(object):
         """
         surface = self.font.render(text, True, (0, 255, 0))
         self.screen.blit(surface, position)
+
+
+class Ball(object):
+
+    def __init__(self, radius=50, colour=(0, 0, 255), x=320, y=240):
+        self.radius = radius
+        self.color  = colour
+        self.x = x
+        self.y = y
+
+        self.surface = pygame.Surface((2 * self.radius, 2 * self.radius))
+        self.surface.set_colorkey((0,0,0))         # make black the transparent color (red,green,blue)
+        pygame.draw.circle(self.surface, colour, (radius, radius), radius)
+        self.radius = self.surface.convert_alpha()
+
+    def blit(self, background):
+        background.blit(self.surface, (self.x, self.y))
 
 
 def main():
