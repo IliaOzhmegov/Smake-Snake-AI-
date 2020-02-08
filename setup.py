da97f183d882b6536c640e0def446f9f39451e23
ilia ozhmegov <elijahozhmegov@gmail.com>
2020-02-08 16:29:59 +0100

    add gradle

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Smake",
+    version='1.0',
+    packages=['src'],
+    url='https://github.com/ElijahOzhmegov/Smake-Snake-AI-',
+    license='MIT',
+    author='ElijahOzhmegov',
+    author_email='ElijahOzhmegov@gmail.com',
+    description='Simple NN plays in snake game.'
+)
